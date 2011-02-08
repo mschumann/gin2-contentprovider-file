cd107735f51214eb9828369e14e283b3912ce864
cmagnus71 <christian.magnus@iqser.com>
2011-02-08 17:13:59 +0000

    Initial Version of File ContentProvider for iQser GIN

---
+++ iqserfileplugin/plugin.filesystem/src/test/java/net/sf/iqser/plugin/filesystem/FilesystemContentProviderTest.java
@@ -0,0 +1,55 @@
+package net.sf.iqser.plugin.filesystem;
+
+import junit.framework.TestCase;
+
+public class FilesystemContentProviderTest extends TestCase {
+
+	public void testDestroy() {
+		fail("Not yet implemented");
+	}
+
+	public void testInit() {
+		fail("Not yet implemented");
+	}
+
+	public void testDoSynchonization() {
+		fail("Not yet implemented");
+	}
+
+	public void testDoHousekeeping() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetBinaryData() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetFile() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetActionsContent() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetContentString() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetContentInputStream() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetContentUrls() {
+		fail("Not yet implemented");
+	}
+
+	public void testOnChangeEventEvent() {
+		fail("Not yet implemented");
+	}
+
+	public void testPerformActionStringContent() {
+		fail("Not yet implemented");
+	}
+
+}
