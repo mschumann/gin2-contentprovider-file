eaaa98399664b9db899aee2051f2d3f78569d2aa
cmagnus71 <christian.magnus@iqser.com>
2011-02-08 18:38:25 +0000

    Add FileParser for Rich Text

---
+++ iqserfileplugin/file-parser/src/test/java/net/sf/iqser/plugin/file/parser/rtf/RtfFileParserTest.java
@@ -0,0 +1,41 @@
+package net.sf.iqser.plugin.file.parser.rtf;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.iqser.core.model.Content;
+
+public class RtfFileParserTest extends TestCase {
+
+	public void testGetContent() throws Exception {
+		RtfFileParser parser = new RtfFileParser();
+		FileInputStream is = new FileInputStream(new File(System.getProperty(
+				"testdata.dir", "./file-parser/testdata")
+				+ "/TestDocument.rtf"));
+		Content content = parser.getContent("TestDocument.rtf", is);
+
+		assertNotNull(content);
+		assertEquals("RTF Document", content.getType());
+
+		assertNotNull(content.getAttributeByName("FILENAME"));
+		assertEquals("TestDocument.rtf",
+				content.getAttributeByName("FILENAME").getValue());
+		assertFalse(content.getAttributeByName("FILENAME").isKey());
+
+		assertNotNull(content.getAttributeByName("TITLE"));
+		assertEquals("TestDocument", content.getAttributeByName("TITLE")
+				.getValue());
+		assertTrue(content.getAttributeByName("TITLE").isKey());
+
+		assertNotNull(content.getFulltext());
+		assertTrue(StringUtils
+				.contains(
+						content.getFulltext(),
+						"Ziel des Dokuments ist es den Prozess der Qualitätssicherung"));
+	}
+
+}
