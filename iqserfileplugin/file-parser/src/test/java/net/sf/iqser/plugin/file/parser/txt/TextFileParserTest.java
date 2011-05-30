47f1275b2c75c103cf53f2a7f7d1fedb0a42f2ef
robertbaban <robertbaban@43f2b38e-3069-4ffc-83f3-88226be6b04b>
2011-05-30 07:37:33 +0000

    add more tests

--- iqserfileplugin/file-parser/src/test/java/net/sf/iqser/plugin/file/parser/txt/TextFileParserTest.java
+++ iqserfileplugin/file-parser/src/test/java/net/sf/iqser/plugin/file/parser/txt/TextFileParserTest.java
@@ -1,7 +1,7 @@
 package net.sf.iqser.plugin.file.parser.txt;
 
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.io.StringBufferInputStream;
 
 import junit.framework.TestCase;
 
@@ -10,25 +10,45 @@ import com.iqser.core.model.Content;
 public class TextFileParserTest extends TestCase {
 
     public void testGetContent() throws Exception {
-	TextFileParser parser = new TextFileParser();
-	String contentSample = "This is the content from the InputStream.";
-	InputStream is = new StringBufferInputStream(contentSample);
-	Content content = parser.getContent("sample.txt", is);
-
-	assertNotNull(content);
-	assertEquals("Text Document", content.getType());
-
-	assertNotNull(content.getAttributeByName("FILENAME"));
-	assertEquals("sample.txt", content.getAttributeByName("FILENAME")
-		.getValue());
-	assertFalse(content.getAttributeByName("FILENAME").isKey());
-
-	assertNotNull(content.getAttributeByName("TITLE"));
-	assertEquals("sample", content.getAttributeByName("TITLE").getValue());
-	assertTrue(content.getAttributeByName("TITLE").isKey());
-
-	assertNotNull(content.getFulltext());
-	assertEquals(contentSample, content.getFulltext());
+		TextFileParser parser = new TextFileParser();
+		String contentSample = "This is the content from the InputStream.";
+		InputStream is = new ByteArrayInputStream(contentSample.getBytes());
+		Content content = parser.getContent("sample.txt", is);
+	
+		assertNotNull(content);
+		assertEquals("Text Document", content.getType());
+	
+		assertNotNull(content.getAttributeByName("FILENAME"));
+		assertEquals("sample.txt", content.getAttributeByName("FILENAME")
+			.getValue());
+		assertFalse(content.getAttributeByName("FILENAME").isKey());
+	
+		assertNotNull(content.getAttributeByName("TITLE"));
+		assertEquals("sample", content.getAttributeByName("TITLE").getValue());
+		assertTrue(content.getAttributeByName("TITLE").isKey());
+	
+		assertNotNull(content.getFulltext());
+		assertEquals(contentSample, content.getFulltext());
     }
 
+    public void testGetContentNullInputStream() throws Exception {
+    	TextFileParser parser = new TextFileParser();    	
+    	InputStream is = null;
+    	Content content = parser.getContent("sample.txt", is);
+
+    	assertNotNull(content);
+    	assertEquals("Text Document", content.getType());
+
+    	assertNotNull(content.getAttributeByName("FILENAME"));
+    	assertEquals("sample.txt", content.getAttributeByName("FILENAME").getValue());
+    	assertFalse(content.getAttributeByName("FILENAME").isKey());
+
+    	assertNotNull(content.getAttributeByName("TITLE"));
+    	assertEquals("sample", content.getAttributeByName("TITLE").getValue());
+    	assertTrue(content.getAttributeByName("TITLE").isKey());
+
+    	assertNotNull(content.getFulltext());
+    	assertEquals("", content.getFulltext());
+   }
+       
 }
